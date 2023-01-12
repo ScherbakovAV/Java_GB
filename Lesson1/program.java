2c70f565b6df2f571de3d2183c917b10f8e73ad0
Scherbakov Anatoly <axidarr@gmail.com>
2023-01-12 19:44:07 +0300

    First commit - Lesson1

---
+++ Lesson1/program.java
@@ -0,0 +1,16 @@
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        String sentense = "Hello, world!";
+        String never = null;
+        Float e = 2.7f;
+        System.out.println(sentense);
+        System.out.println(never);
+        System.out.println(e);
+        int num = 123;
+        num = num-- - --num;
+        System.out.println(num);
+    }
+}
